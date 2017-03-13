5acf8e33b278d8486e56f9c962c9d40c15776e63
Natnael Getahun <ngetahun@users.noreply.github.com>
2017-03-14 00:00:48 +0100

    added fixes
    Added a check to see if directories existed

--- tmux-zsh/config.sh
+++ tmux-zsh/config.sh
@@ -12,13 +12,16 @@ sudo apt-get install git zsh tmux fish -y
 sudo chsh -s /bin/zsh
 
 # install oh-my-zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 
 # install vim bundle manager vundle
-mkdir -p ~/.vim
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    mkdir -p ~/.vim
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 # move zsh, tmux and vim configuration files
 cd conf_files
